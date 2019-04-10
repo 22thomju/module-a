9914862e70619f046d6be081a1e31c7da7bf8fe3
22thomju <49483189+22thomju@users.noreply.github.com>
2019-04-10 12:40:51 -0400

    adding hello-world.py
    adding file

---
+++ hello-world.py
@@ -0,0 +1,12 @@
+# Julia
+# 04/10/19
+
+########################
+#     Hello World      #
+#                      #
+#   Our first python   #
+#     Application      #
+########################
+
+# print "hello, world" to the screen.
+print("Hello, world") 
